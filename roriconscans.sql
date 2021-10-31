b796fbae352879a3a65a50160af6e2c31d671a4e
saintly2k <sejo.gaming.com@gmail.com>
2021-10-31 23:02:52 +0100

    forgot to upload lol

---
+++ roriconscans.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 31, 2021 at 06:48 PM
+-- Server version: 5.7.36-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `roriconscans`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chapters`
+--
+
+CREATE TABLE `chapters` (
+  `id` int(11) NOT NULL,
+  `manga` int(11) NOT NULL,
+  `url` varchar(25) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `chapter` int(4) NOT NULL,
+  `images` varchar(25) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chapters`
+--
+
+INSERT INTO `chapters` (`id`, `manga`, `url`, `title`, `chapter`, `images`, `date`) VALUES
+(1, 1, 'JQub2-mlkad-I9gFd-WuIoX', 'Unfit to be a dog', 10, '12345-12435-12345-12345', '2021-10-29 22:24:02'),
+(2, 1, 'JQub2-mlkcd-wafRT-WuIoX', 'An inquiry', 11, '23456-23456-23456-23456', '2021-10-29 22:25:44');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mangas`
+--
+
+CREATE TABLE `mangas` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `cover` varchar(25) NOT NULL,
+  `alternates` varchar(250) DEFAULT NULL,
+  `scanlating` tinyint(1) NOT NULL DEFAULT '1',
+  `url` varchar(25) NOT NULL,
+  `description` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mangas`
+--
+
+INSERT INTO `mangas` (`id`, `title`, `cover`, `alternates`, `scanlating`, `url`, `description`) VALUES
+(1, 'Pochi Gokko', 'sHiVdShXIrIu8al.jpeg', NULL, 1, 'JQub2-mlkcd-I9gFd-W6ziC', 'A promising romcom from Weekly Young Jump, the first taste of fluff in the Reiwa era.\nOne day, the protagonist Tomoyori decides to end his own life.\nBut that was not the end - as his eyes opened, he found himself in a strange room.\nTurns out, he\'s now the pet of a mysterious high school girl. Yes, you heard that right.\nInteracting with a high school girl? Could this be a gift from the heavens? Or is it divine punishment?\nA new era, a new beginning!');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chapters`
+--
+ALTER TABLE `chapters`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mangas`
+--
+ALTER TABLE `mangas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chapters`
+--
+ALTER TABLE `chapters`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `mangas`
+--
+ALTER TABLE `mangas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
