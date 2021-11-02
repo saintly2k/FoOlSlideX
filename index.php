842954c5b8594a8577032b5ee8424635f149e174
saintly2k <sejo.gaming.com@gmail.com>
2021-11-02 21:44:30 +0100

    issues with pagination fixed

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ require("core/db.php");
 require("core/config.php");
 require("core/overlord.php");
 
-$page = $_GET["page"];
-if(!isset($page)) {
-    $page = $config["home"];
+$hh = $_GET["page"];
+if(!isset($hh)) {
+    $hh = $config["home"];
 }
 
 include("views/templates/header.php");
 include("views/templates/menu.php");
-include("views/pages/$page.php");
+include("views/pages/$hh.php");
 include("views/templates/footer.php");
 
 ?>
