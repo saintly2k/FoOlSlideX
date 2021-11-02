1dcd64daf7b8a5d42ec4a544a6e210f97ff11e23
saintly2k <sejo.gaming.com@gmail.com>
2021-11-02 20:57:49 +0100

    fixed unable to upload

--- views/pages/add.php
+++ views/pages/add.php
@@ -15,11 +15,11 @@ if(isset($_SESSION["username"])) { ?>
                 <?php include("views/pages/mini/upload.php"); ?>
             </div>
             <div class="col-8">
-                <input type="text" name="manga_title" style="width:100%;" required placeholder="[Required] Manga Title"><br><br>
+                <input type="text" name="manga_title" style="width:100%;" placeholder="[Required] Manga Title"><br><br>
                 <textarea type="textfield" name="manga_alternates" style="width:100%;" placeholder="Alternative Titles (Other Languages, Romaji, Original Japanese, etc)"></textarea><br><br>
                 <input type="text" name="manga_scanlating" style="width:100%;" placeholder="[Required] Status of scanlation (1 for Scanlating, 0 for Dropped or Finished)"><br><br>
                 <textarea type="textfield" name="manga_description" style="width:100%;" placeholder="Description of Manga"></textarea><br><br>
-                <input required value="<?php echo $kami; ?>" style="display:none" name="manga_cover">
+                <input value="<?php echo $kami; ?>" style="display:none" name="manga_cover">
             </div>
             <div class="col-12">
                 <input type="submit" value="Add Manga!" name="add-manga" style="width:100%">
