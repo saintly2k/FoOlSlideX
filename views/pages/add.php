5c10f8b4c1f743a665c6a2245cc961bbd67643ac
saintly2k <sejo.gaming.com@gmail.com>
2021-11-02 20:47:10 +0100

    redo of layout

--- views/pages/add.php
+++ views/pages/add.php
@@ -8,21 +8,29 @@ if(isset($_SESSION["username"])) { ?>
 
     <h3>Add Manga</h3>
 
-    <?php include("views/pages/mini/upload.php"); ?>
 
     <form name="add-manga" method="post" action="">
-        Manga Title: <input type="text" name="manga_title"><br>
-        Alternative Names: <textarea type="textfield" name="manga_alternates"></textarea><br>
-        Scanlting Currently (1 for yes, 0 for no): <input type="text" name="manga_scanlating"><br>
-        Description: <textarea type="textfield" name="manga_description"></textarea><br>
-        <input required value="<?php echo $kami; ?>" style="display:none" name="manga_cover">
-        <input type="submit" value="Add Manga!" name="add-manga">
+        <div class="row">
+            <div class="col-4">
+                <?php include("views/pages/mini/upload.php"); ?>
+            </div>
+            <div class="col-8">
+                <input type="text" name="manga_title" style="width:100%;" required placeholder="[Required] Manga Title"><br><br>
+                <textarea type="textfield" name="manga_alternates" style="width:100%;" placeholder="Alternative Titles (Other Languages, Romaji, Original Japanese, etc)"></textarea><br><br>
+                <input type="text" name="manga_scanlating" style="width:100%;" placeholder="[Required] Status of scanlation (1 for Scanlating, 0 for Dropped or Finished)"><br><br>
+                <textarea type="textfield" name="manga_description" style="width:100%;" placeholder="Description of Manga"></textarea><br><br>
+                <input required value="<?php echo $kami; ?>" style="display:none" name="manga_cover">
+            </div>
+            <div class="col-12">
+                <input type="submit" value="Add Manga!" name="add-manga" style="width:100%">
+            </div>
+        </div>
     </form>
 
 </div>
 
 <?php } else {
-    echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
+    echo "<script type='text/javascript'> document.location = 'index.php?page=login'; </script>";
 }
 
 ?>
