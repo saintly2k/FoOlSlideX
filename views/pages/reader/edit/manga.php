b4baa0738d45a3962206aa0468f93cd717f09ebd
saintly2k <ninefreaks@yandex.com>
2021-11-05 20:02:08 +0100

    fixed description & alternate titles not editable

--- views/pages/reader/edit/manga.php
+++ views/pages/reader/edit/manga.php
@@ -10,13 +10,13 @@ if ($editResult->num_rows > 0) {
         $mID = $erow["id"];
         $mTITLE = $erow["title"];
         $mCOVER = $erow["cover"];
-        if(empty($kami)) {
-            $kami = $mCOVER;
-        }
         $mALTER = $erow["alternates"];
         $mSCANL = $erow["scanlating"];
         $mURL = $erow["url"];
         $mDESC = $erow["description"];
+        if(empty($kami)) {
+            $kami = $mCOVER;
+        }
     }
 } else {
     echo "Manga Error";
@@ -30,9 +30,9 @@ if ($editResult->num_rows > 0) {
         <div class="col-9">
             <h3>Update Manga Info for <a href="?page=view&manga=<?php echo $mURL; ?>"><?php echo $mTITLE; ?></a></h3>
             <input type="text" name="manga_title" placeholder="Manga Title" value="<?php echo $mTITLE; ?>"><br><br>
-            <textarea name="manga_alternates" placeholder="Alternate Manga Titles" value="<?php echo $mALTER; ?>" style="width:100%;"></textarea><br><br>
+            <textarea name="manga_alternates" placeholder="Alternate Manga Titles" style="width:100%;"><?php echo $mALTER; ?></textarea><br><br>
             <input type="text" name="manga_scanlating" placeholder="Scanlation Status (1 for Scanlating, 0 for Dropped/Finished)" value="<?php echo $mSCANL; ?>"><br><br>
-            <textarea name="manga_description" placeholder="Manga Description" value="<?php echo $mDESC; ?>" style="width:100%;"></textarea><br><br>
+            <textarea name="manga_description" placeholder="Manga Description" style="width:100%;"><?php echo $mDESC; ?></textarea><br><br>
             <input type="text" name="manga_url" value="<?php echo $mURL; ?>" style="display:none"><br><br>
             <input type="text" name="manga_cover" value="<?php echo $kami; ?>" style="display:none"><br><br>
         </div>
