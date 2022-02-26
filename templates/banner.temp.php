51284b98a30a8f30844c11f0c9af2244c2507e9e
Selim B <ninefreaks@yandex.com>
2022-02-26 13:35:34 +0100

    Delete templates directory

--- templates/banner.temp.php
+++
@@ -1,5 +0,0 @@
-<div class="panel panel-default">
-    <div class="panel-body">
-        Banner here soon™
-    </div>
-</div>
--- templates/sidebar.temp.php
+++
@@ -1,8 +0,0 @@
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title">Sidebar</h3>
-    </div>
-    <div class="panel-body">
-        Idk what should be here?
-    </div>
-</div>
