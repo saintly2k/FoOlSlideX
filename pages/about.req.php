bba5d4913936bef2f1bc78078a21af53ddcda21b
Selim B <ninefreaks@yandex.com>
2022-02-26 13:35:52 +0100

    Delete pages directory

--- pages/about.req.php
+++
@@ -1,9 +0,0 @@
-<title>About | <?= $config["name"] ?></title>
-<div class="panel panel-success">
-    <div class="panel-heading">
-        <h3 class="panel-title">About <?php echo $config["name"]; ?></h3>
-    </div>
-    <div class="panel-body">
-        This file has not been edited yet (hint: it's located in <b>pages/about.req.php</b>. It can be used for the team-page, your members, your schedule or whatsoever.
-    </div>
-</div>
