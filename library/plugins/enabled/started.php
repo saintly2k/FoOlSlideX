ceba1000e37c91fcf4362626339744e063055a5a
saintly2k <ninefreaks@yandex.com>
2023-05-30 00:12:25 +0200

    Moved plugins location

--- library/plugins/enabled/started.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-if (!file_exists(ps(__DIR__ . "/../../secrets/started.txt"))) die("started.txt is missing. did you install the software properly?");
-$started = file_get_contents(ps(__DIR__ . "/../../secrets/started.txt"));
